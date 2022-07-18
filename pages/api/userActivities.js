a6dba258fbfd38b64f09fe15b129f84806f106a5
BuildTools <unconfigured@null.spigotmc.org>
2022-07-18 14:38:03 -0400

    userActivities update db, fix excessive connections

--- pages/api/userActivities.js
+++ pages/api/userActivities.js
@@ -1,63 +1,115 @@
 import clientPromise from "../../lib/mongodb";
 import { GetAccessToken } from "./refreshTokens";
 
+// format for storing activities:
+// activity = {
+//     activityId: id,
+//     name: name,
+//     distance: distance,
+//     movingTime: moving_time,
+//     elapsedTime: elapsed_time,
+//     elevGain: total_elevation_gain,
+//     elevHigh: elev_high,
+//     elevLow: elev_low,
+//     startDateLocal: start_date_local,
+//     utcOffset: utc_offset,
+//     startLatLng: start_latlng,
+//     endLatLng: end_latlng,
+//     city: location_city,
+//     state: location_state,
+//     country: location_country,
+//     kudos: kudos_count,
+//     groupSize: athlete_count,
+//     map: map,
+//     prs: pr_count
+// }
+
 async function UpdateActivities(athleteId, accessToken) {
     // query DB
     const client = await clientPromise;
     const db = client.db(process.env.DB);
     const userActivities = db.collection("user_activities");
 
+    // find existing entry
     const existing = await userActivities.findOne({ id: athleteId });
-    // 12AM EST 6/21/2022 (1655784000)
-    const startTime = 1635784000
+    // get current epoch timestamp
     const curTime = Math.floor(Date.now() / 1000);
 
     // create activities entry if it doesn't exist, or replace completely if the data is more than four hours old
     let activities = [];
 
-    console.log(existing);
-
-    if (!existing || (existing.lastUpdated + 14400) < curTime) {
-        if (!existing) {
-            const returnActivities = async (page) => {
-                const activitiesURL = `https://www.strava.com/api/v3/athlete/activities?before=${curTime}&after=${startTime}&page=${page}&per_page=200&access_token=${accessToken}`;
-                const res = await fetch(activitiesURL);
-                const activitiesResponseJSON = await res.json();
-                return activitiesResponseJSON;
+    // return given page of activities (since limit is 200/page)
+    const returnActivities = async (page, startTime) => {
+        const activitiesURL = `https://www.strava.com/api/v3/athlete/activities?before=${curTime}&after=${startTime}&page=${page}&per_page=200&access_token=${accessToken}`;
+        const res = await fetch(activitiesURL);
+        const activitiesResponseJSON = await res.json();
+        return activitiesResponseJSON;
+    }
+    // only try first 5 pages - 1000 activities
+    const getActivities = async (startTime) => { 
+        for (let i = 1; i <= 5; i++) {
+            const promise = await returnActivities(i, startTime);
+            // break if the array returned is empty to save requests
+            if (!(Array.isArray(promise) && promise.length)) {
+                break;
             }
-
-            // only try first 5 pages - 1000 activities
-            for (let i = 1; i <= 5; i++) {
-                const promise = await returnActivities(i);
-                // break if the array returned is empty to save requests
-                if (!(Array.isArray(promise) && promise.length)) {
-                    break;
-                }
-                await activities.push(promise[0].pr_count);
+            await activities.push(promise);
+        }
+        activities = await(activities.flat());
+        const formattedActivities = activities.map((activity=value) => (
+            {
+                activityId: activity.id,
+                name: activity.name,
+                distance: activity.distance,
+                movingTime: activity.moving_time,
+                elapsedTime: activity.elapsed_time,
+                elevGain: activity.total_elevation_gain,
+                elevHigh: activity.elev_high,
+                elevLow: activity.elev_low,
+                startDateLocal: activity.start_date_local,
+                utcOffset: activity.utc_offset,
+                startLatLng: activity.start_latlng,
+                endLatLng: activity.end_latlng,
+                city: activity.location_city,
+                state: activity.location_state,
+                country: activity.location_country,
+                kudos: activity.kudos_count,
+                groupSize: activity.athlete_count,
+                map: activity.map,
+                prs: activity.pr_count
             }
-            await activities.flat();
-        } else {
+        ));
+        return formattedActivities;
+    }
 
-        }
-        // get required data from Strava
-        
-        // update DB with new clubs, if user doesn't exist create a new entry.
-        const activitiesDBFilter = {
-            id: athleteId
-        }
-        const activitiesDBData = {
-            $set: {
-                id: athleteId,
-                lastUpdated: curTime,
-                activities: activities
-            }
-        }
-
-        // await userActivities.findOneAndUpdate(activitiesDBFilter, activitiesDBData, { upsert: true });
+    // logic if data is old or just doesn't exist
+    if (!existing) {
+        // 12AM EST 6/21/2022 (1655784000)
+        const startTime = 1655784000
+        await getActivities(startTime);
+    } else if ((existing.lastUpdated + 86400) < curTime) {
+        // get activities from last updated timestamp
+        const startTime = existing.lastUpdated;
+        await getActivities(startTime);
     } else {
+        // return database activities, don't query db or strava API
         return existing.activities;
     }
+
+    // update DB with new clubs, if user doesn't exist create a new entry.
+    const activitiesDBFilter = {
+        id: athleteId
+    }
+    const activitiesDBData = {
+        $set: {
+            id: athleteId,
+            lastUpdated: curTime,
+            activities: activities
+        }
+    }
+    await userActivities.findOneAndUpdate(activitiesDBFilter, activitiesDBData, { upsert: true });
 }
+
 export default async function UserActivities(req, res) {
     if (req.method != 'POST') {
         return res.status(405).send({ message: "You may only send POST requests to this endpoint. "});
