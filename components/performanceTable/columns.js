072460424575f58dda8cc647b296637932de4e9b
BuildTools <unconfigured@null.spigotmc.org>
2022-08-17 20:11:41 -0400

    Add styling to date in performance table

--- components/performanceTable/columns.js
+++ components/performanceTable/columns.js
@@ -1,8 +1,28 @@
+import styles from '../../styles/PerformanceTable.module.css'
+
 export const athleteColumns = [
     {
         Header: 'Date',
         accessor: 'date',
-        width: '200px'
+        width: '200px',
+        Cell: ({ value }) => {
+            const options = {
+                "month": "numeric",
+                "day": "numeric",
+                "year": "2-digit"
+            }
+
+            const date = (new Date(value)).toLocaleDateString("en-US", options);
+            const days = Math.floor((Date.now() - Date.parse(value)) / (1000 * 60 * 60 * 24));
+            return (
+                <>
+                    <div className={styles.dateColumn}>
+                        <div className={styles.date}>{date}</div>
+                        <div className={styles.timeAgo}>({days}d ago)</div>
+                    </div>
+                </>
+            );
+        }
     },
     {
         Header: 'Title',
@@ -14,7 +34,7 @@ export const athleteColumns = [
         accessor: 'distance',
         width: '100px',
         Cell: ({ value }) => {
-            return (<div>{Math.round(value * 0.06214) / 100}mi</div>)
+            return (<div>{Math.round(value * 0.06214) / 100}mi</div>);
         }
     },
     {
