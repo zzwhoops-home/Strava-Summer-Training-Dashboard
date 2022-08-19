d723ca647cdc75d3e9e0a9acd128b5b64466dfbe
BuildTools <unconfigured@null.spigotmc.org>
2022-08-19 00:36:51 -0400

    Add styling for performance table, column cleanup

--- components/performanceTable/columns.js
+++ components/performanceTable/columns.js
@@ -27,35 +27,34 @@ export const athleteColumns = [
     {
         Header: 'Title',
         accessor: 'title',
-        width: '250px'
-    },
-    {
-        Header: 'Distance',
-        accessor: 'distance',
-        width: '100px',
-        Cell: ({ value }) => {
-            return (<div>{Math.round(value * 0.06214) / 100}mi</div>);
+        width: '400px',
+        Cell: ({ row }) => {
+            const activity = row.original;
+            const link = `https://www.strava.com/activities/${activity.activityId}`;
+            
+            return (
+                <div className={styles.title}>
+                    <a href={link}>
+                        {activity.title}
+                    </a>
+                </div>
+            );
         }
-    },
-    {
-        Header: 'Elevation Gain',
-        accessor: 'elevGain',
-        width: '100px'
-    },
-    {
-        Header: 'Kudos',
-        accessor: 'kudos',
-        width: '25px'
     },
     {
         Header: 'Performance',
         accessor: 'performance',
-        width: '175px',
-        Cell: ({ value, row }) => {
-            const weighted = value * Math.pow(0.95, row.index);
+        width: '250px',
+        Cell: ({ row }) => {
+            const activity = row.original;
+            const performance = activity.performance;
+
+            const weighted = performance * Math.pow(0.95, row.index);
             return (
                 <>
-                    <div>{Math.round(value)}pp ({Math.round(weighted)}pp)</div>
+                    <div className={styles.performanceColumn}>
+                        <div>{Math.round(performance)}pp ({Math.round(weighted)}pp)</div>
+                    </div>
                 </>
             );
         }
